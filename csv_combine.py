00b7aa735287e59b2873dbb8d05c2df8eb9a532b
varens <stan@leopolitan.net>
2023-06-18 17:17:53 +0900

    Allow dirs as xml source.

--- csv_combine.py
+++ csv_combine.py
@@ -1,5 +1,7 @@
 import os
 import csv
+import sqlite3
+from typing import Iterator
 
 def combine_csv_files(file_paths, output_filename):
     combined_data = []
@@ -20,16 +22,35 @@ def combine_csv_files(file_paths, output_filename):
 
     print(f"Combined CSV files saved as {output_filename}")
 
+def get_ports(xml_path: str) -> Iterator[list]:
+    """
+    Retrieves port data from an nmap xml.
+    """
+    pass
+
+def list_files(file_path: str) -> set[str]:
+    """
+    Retrieves absolute path(s) for a given file or directory of xml files.
+    """
+    if os.path.isfile(file_path):
+        return set(os.path.abspath(file_path))
+    else:
+        return set([
+            os.path.abspath(f) 
+            for f in os.listdir(file_path)
+            if f.lower().endswith('.xml')
+        ])
+
 if __name__ == '__main__':
-    file_paths = []
+    file_paths = set()
     while True:
-        file_path = input("Enter the path of the CSV file to combine (or 'done' to finish): ")
+        file_path = input("Enter the path of the XML file or directory to combine (or 'done' to finish): ")
         if file_path.lower() == 'done':
             break
         if not os.path.exists(file_path):
-            print("File not found. Please enter a valid file path.")
+            print("File/dir not found. Please enter a valid file path.")
             continue
-        file_paths.append(file_path)
+        file_paths |= list_files(file_path)
 
     output_filename = input("Enter the filename for the combined CSV file: ")
     combine_csv_files(file_paths, output_filename)
